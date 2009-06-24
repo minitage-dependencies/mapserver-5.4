1c3703392fcf0ffbefe2a0eda8e6ec009e4bf3f7
kiorky <kiorky@cryptelium.net>
2009-06-24 16:16:48 +0200

    fixmapserver

--- hooks/hook.py
+++ hooks/hook.py
@@ -13,17 +13,25 @@ def hook(options,buildout):
              )
 
 def install(options, buildout):
-    location = os.path.join(
-        options['location'], 'bin')
-    if not os.path.isdir(location):
-        os.makedirs(location)
+    location = os.path.join( options['location'], 'bin')
+    include = os.path.join( options['location'], 'include')
+    lib = os.path.join( options['location'], 'lib')
+    for p in location, include, lib:
+        if not os.path.isdir(p):
+            os.makedirs(p)
     mapserv = os.path.join(
         options['compile-directory'], 'mapserv')
+    c = options['compile-directory']
     dmapserv = os.path.join( location, 'mapserv')
     shutil.copy(mapserv, dmapserv)
     message =  'Successfully installed mapserv into %s'
     logging.getLogger('mapserver hook').info(
         message % location)
 
+    [shutil.copy(
+        os.path.join(c,f), include
+    )
+        for f in os.listdir(c)
+        if f.endswith('.h')]
 
 
