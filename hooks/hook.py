67e9ede86c4435d4fdfe7280e4dbee678cf90d77
makina <makina@APEM3.(none)>
2009-11-30 11:36:55 +0100

    win32port

--- hooks/hook.py
+++ hooks/hook.py
@@ -1,6 +1,9 @@
 import os
+import sys
 import shutil
 import logging
+import re
+
 def hook(options,buildout):
     """"""
     os.system("""
@@ -40,3 +43,29 @@ def install(options, buildout):
     #    if f.startswith('.libmap')] 
 
 
+def p(o, b):
+    lib_re = re.compile('^((lib)(?P<dll>.+))$')
+    if 'win' in sys.platform:
+        dest = o['location']
+        bin = os.path.join(dest, 'bin')
+        lib = os.path.join(dest, 'lib')
+        if not os.path.exists(bin): os.makedirs(bin)
+        for dll in [libdll 
+                    for libdll in os.listdir(lib) 
+                    if( libdll.endswith('dll') 
+                    and libdll.startswith('lib'))]:
+            ldests = [os.path.join(bin, dll)]
+            orig = os.path.join(lib, dll)
+            m = lib_re.match(dll)
+            if m:
+                dg = m.groupdict()
+                ldests.append(os.path.join(bin, 'cyg%s' % dg['dll']))
+            for ldest in ldests:
+                if os.path.exists(ldest):
+                    os.remove(ldest)
+                
+                shutil.copy2(orig, ldest)
+    
+    
+    
+    
